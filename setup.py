1ed26f6184eb564dd18249a7168c2f096f6cb925
Prasanna Swaminathan <prasanna@pswaminathan.com>
2013-05-13 22:24:02 -0400

    Replace setup.sh with setup.py -- covers same bases, setuptools cmpatible.

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
+
+packages = ['t1apicore']
+
+requirements = ['requests']
+
+setup(name='t1apicore',
+		version=t1apicore.__version__,
+		author='Prasanna Swaminathan',
+		author_email='pswaminathan@mediamath.com',
+		url='http://www.mediamath.com',
+		description="A package for interacting with MediaMath's TerminalOne API."
+		long_desription=open('README.md').read()
+		packages=packages,
+		install_requires=requrements,
+		platforms=['any'],
+		license='MIT',
+)
