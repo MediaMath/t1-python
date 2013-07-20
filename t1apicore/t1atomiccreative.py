0887f98615807a27788b6d9704423c04cd41785b
Prasanna Swaminathan <prasanna@pswaminathan.com>
2013-07-20 17:33:14 -0400

    Complete atomic creative type conversions.

--- t1apicore/t1atomiccreative.py
+++ t1apicore/t1atomiccreative.py
@@ -15,45 +15,99 @@ class T1AtomicCreative(T1Object):
 	def __init__(self):
 		super(T1AtomicCreative, self).__init__()
 		self.collection = 'atomic_creatives'
-		self._ad_formats = self._valid_enum(frozenset(['DISPLAY', 'EXPANDABLE', 'MOBILE']), 'DISPLAY')
-		self._ad_servers = self._valid_enum(frozenset(['ATLAS', 'DART', 'EYEWONDER', 'MEDIAMIND',
+		self._readonly.update({'t1as', 'built'})
+		self._ad_formats = self._enum({'DISPLAY', 'EXPANDABLE', 'MOBILE'}, 'DISPLAY')
+		self._ad_servers = self._enum('ATLAS', 'DART', 'EYEWONDER', 'MEDIAMIND',
 									'MEDIAPLEX', 'POINTROLL', 'YIELD_MANAGER',
-									'TERMINALONE', 'MEDIAFORGE', 'OTHER']), 'OTHER')
-		self._approved = self._valid_enum(frozenset(['PENDING', 'APPROVED', 'REJECTED']), 'PENDING')
-		self._expand_dir = self._valid_enum(frozenset(['L', 'R', 'U', 'D', 'LD', 'RD', 'LU',
-											'RU', 'NONRESTRICTED']), 'NONRESTRICTED')
-		self._expand_trig = self._valid_enum(frozenset(['AUTOMATIC', 'MOUSEOVER', 'CLICK']), 'CLICK')
-		self._file_types = self._valid_enum(frozenset(['gif', 'jpg', 'swf', 'unknown']), 'unkown')
-		self._tag_types = self._valid_enum(frozenset(['SCRIPT', 'IFRAME', 'NOSCRIPT']), 'NOSCRIPT')
-		self._readonly.update({'t1as'})
-		self._types.update({'advertiser_id': int, 'concept_id': int,
-			'external_identifier': str, 'file_type': str, 'tag_type': str,
-			'width': int, 'height': int, 'is_https': str, 'has_sound': str,
-			'is_multi_creative': str, 'ad_server_type': str, 'tag': str,
-			'tpas_ad_tag_name': str, 'edited_tag': str, 'tpas_ad_tag': str,
-			'ad_format': str, 'approval_status': str, 'build_errors': str,
-			'built': bool, 'built_by_user_id': int, 'click_through_url': str,
-			'click_url': str, 'creative_import_file_id': int, 'end_date': datetime,
-			'expansion_direction': str, 'expansion_trigger': str, 'rejected_reason': str,
-			'rich_media': bool, 'rich_media_provider': str, 'start_date': datetime,
-			't1as': bool
-			})
-		self._convert_lambda = lambda x, y, z: x if str(x) in y else z
-		# What I really want here is a closure. Want to freeze y and z when I assign
-		# in _convert, and x is what gets passed on function call
-		self._convert.update({'advertiser_id': int, 'concept_id': int,
-			'file_type': self._file_types, 'tag_type': self._tag_types,
-			'ad_format': self._ad_formats, 'ad_server_type': self._ad_servers,
-			'width': int, 'height': int, 'is_https': self._bool_deffalse, 'external_identifier': str,
-			'has_sound': self._bool_deffalse, 'is_multi_creative': self._bool_deffalse,
-			'tag': str, 'tpas_ad_tag_name': str,
-			'edited_tag': str, 'tpas_ad_tag': str, 'approval_status': self._approved,
-			'build_errors': str, 'built': self._int_to_bool, 'built_by_user_id': int,
-			'click_through_url': str, 'click_url': str, 'creative_import_file_id': int,
-			'end_date': self._strpt, 'expansion_direction': self._expand_dir,
-			'expansion_trigger': self._expand_trig, 'rejected_reason': str,
-			'rich_media': self._int_to_bool, 'rich_media_provider': str,
-			'start_date': self._strpt, 't1as': self._int_to_bool})
+									'TERMINALONE', 'MEDIAFORGE', 'OTHER'}, 'OTHER')
+		self._approved = self._enum({'PENDING', 'APPROVED', 'REJECTED'}, 'PENDING')
+		self._expand_dir = self._enum({'L', 'R', 'U', 'D', 'LD', 'RD', 'LU',
+											'RU', 'NONRESTRICTED'}, 'NONRESTRICTED')
+		self._expand_trig = self._enum({'AUTOMATIC', 'MOUSEOVER', 'CLICK'}, 'CLICK')
+		self._file_types = self._enum({'gif', 'jpg', 'swf', 'unknown'}, 'unkown')
+		self._tag_types = self._enum({'SCRIPT', 'IFRAME', 'NOSCRIPT'}, 'NOSCRIPT')
+		self._pull.update({
+			'advertiser_id': int,
+			'ad_format': str,
+			'ad_server_type': str,
+			'approval_status': str,
+			'build_errors': str,
+			'built': self._int_to_bool,
+			'built_by_user_id': int,
+			'click_through_url': str,
+			'click_url': str,
+			'concept_id': int,
+			'creative_import_file_id': int,
+			'edited_tag': str,
+			'end_date': self._strpt,
+			'expansion_direction': str,
+			'expansion_trigger': str,
+			'external_identifier': str,
+			'file_type': str,
+			'has_sound': self._int_to_bool,
+			'height': int,
+			'is_https': self._int_to_bool,
+			'is_multi_creative': self._int_to_bool,
+			'rejected_reason': str,
+			'rich_media': self._int_to_bool,
+			'rich_media_provider': str,
+			'start_date': self._strpt,
+			't1as': self._int_to_bool,
+			'tag': str,
+			'tag_type': str,
+			'tpas_ad_tag': str,
+			'tpas_ad_tag_name': str,
+			'width': int,
+		})
+		self._push = self._pull.copy()
+		self._push.update({
+			'ad_format': self._ad_formats,
+			'ad_server_type': self._ad_servers,
+			'approval_status': self._approved,
+			'end_date': self._strft,
+			'expansion_direction': self._expand_dir,
+			'expansion_trigger': self._expand_trig,
+			'file_type': self._file_types,
+			'has_sound': int,
+			'is_https': int,
+			'is_multi_creative': int,
+			'rich_media': int,
+			'start_date': self._strft,
+			't1as': int,
+			'tag_type': self._tag_types,
+		})
 		
 		
 		pass
+# self._types.update({'advertiser_id': int,
+# 			'ad_format': str,
+# 			'ad_server_type': str,
+# 			'approval_status': str,
+# 			'build_errors': str,
+# 			'built': bool,
+# 			'built_by_user_id': int,
+# 			'click_through_url': str,
+# 			'click_url': str,
+# 			'concept_id': int,
+# 			'creative_import_file_id': int,
+# 			'edited_tag': str,
+# 			'end_date': datetime,
+# 			'expansion_direction': str,
+# 			'expansion_trigger': str,
+# 			'external_identifier': str,
+# 			'file_type': str,
+# 			'has_sound': str,
+# 			'height': int,
+# 			'is_https': str,
+# 			'is_multi_creative': str,
+# 			'rejected_reason': str,
+# 			'rich_media': bool,
+# 			'rich_media_provider': str,
+# 			'start_date': datetime,
+# 			't1as': bool,
+# 			'tag': str,
+# 			'tag_type': str,
+# 			'tpas_ad_tag': str,
+# 			'tpas_ad_tag_name': str,
+# 			'width': int,
+# 			})
