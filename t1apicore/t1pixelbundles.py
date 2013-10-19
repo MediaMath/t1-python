a34e2fe69d0d7dc009b573b5a134fafcdc0be0d2
Prasanna Swaminathan <pswaminathan@mediamath.com>
2013-10-19 19:27:33 -0400

    Make push and pull class attributes, as they don't change with instances.

--- t1apicore/t1pixelbundles.py
+++ t1apicore/t1pixelbundles.py
@@ -11,48 +11,52 @@ from .t1object import T1Object
 
 class T1PixelBundle(T1Object):
 	"""docstring for T1PixelBundle"""
-	def __init__(self):
-		super(T1PixelBundle, self).__init__()
-		self.collection = 'pixel_bundles'
-		self._readonly.update({'pixel_type', 'tags', 'external_identifier',
-								'tag_type'})
-		self._pixel_types = self._enum({'creative', 'event', 'data', 'segment'},
-										'event')
-		self._pricing = self._enum({'CPM', 'CPTS'}, 'CPM')
-		self._rmx_conv_types = self._enum({'one', 'variable'}, 'one')
-		self._tag_types = self._enum({'dfa', 'uat', 'image', 'iframe', 'js'},
-										'image')
-		del self._pull['status']
-		self._pull.update({
-			'advertiser_id': int,
-			'agency_id': int,
-			'cost_cpm': float,
-			'cost_cpts': float,
-			'cost_pct_cpm': float,
-			'eligible': self._int_to_bool,
-			'external_identifier': str,
-			'pixel_type': str,
-			'pricing': str,
-			'provider_id': int,
-			'rmx_conversion_minutes': int,
-			'rmx_conversion_type': str,
-			'rmx_friendly': self._int_to_bool,
-			'rmx_merit': self._int_to_bool,
-			'rmx_pc_window_minutes': int,
-			'rmx_pv_window_minutes': int,
-			'segment_op': str,
-			'tag_type': str,
-			'tags': str,
-		})
-		self._push = self._pull.copy()
-		self._push.update({
-			'eligible': int,
-			'pixel_type': self._pixel_types,
-			'pricing': self._pricing,
-			'rmx_conversion_type': self._rmx_conv_types,
-			'rmx_friendly': int,
-			'rmx_merit': int,
-			'tag_type': str,
-		})
-		
+	collection = 'pixel_bundles'
+	type = 'pixel_bundle'
+	_pixel_types = T1Object._enum({'creative', 'event', 'data', 'segment'},
+									'event')
+	_pricing = T1Object._enum({'CPM', 'CPTS'}, 'CPM')
+	_rmx_conv_types = T1Object._enum({'one', 'variable'}, 'one')
+	_tag_types = T1Object._enum({'dfa', 'uat', 'image', 'iframe', 'js'}, 'image')
+	_pull = {
+		'advertiser_id': int,
+		'agency_id': int,
+		'cost_cpm': float,
+		'cost_cpts': float,
+		'cost_pct_cpm': float,
+		'created_on': T1Object._strpt,
+		'eligible': T1Object._int_to_bool,
+		'external_identifier': str,
+		'id': int,
+		'name': str,
+		'pixel_type': str,
+		'pricing': str,
+		'provider_id': int,
+		'rmx_conversion_minutes': int,
+		'rmx_conversion_type': str,
+		'rmx_friendly': T1Object._int_to_bool,
+		'rmx_merit': T1Object._int_to_bool,
+		'rmx_pc_window_minutes': int,
+		'rmx_pv_window_minutes': int,
+		'segment_op': str,
+		'tag_type': str,
+		'tags': str,
+		'type': 'pixel_bundle',
+		'updated_on': T1Object._strpt,
+		'version': int,
+	}
+	_push = _pull.copy()
+	_push.update({
+		'eligible': int,
+		'pixel_type': _pixel_types,
+		'pricing': _pricing,
+		'rmx_conversion_type': _rmx_conv_types,
+		'rmx_friendly': int,
+		'rmx_merit': int,
+		'tag_type': _tag_types,
+	})
+	_readonly = T1Object._readonly.copy()
+	_readonly.update({'tags', 'external_identifier',})
+	def __init__(self, auth, properties=None):
+		super(T1PixelBundle, self).__init__(auth, properties)
 		pass
