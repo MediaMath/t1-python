6502e56f56aa7adcc4406d0ff7a98f8ba99ff338
Prasanna Swaminathan <pswaminathan@mediamath.com>
2014-01-13 10:23:26 -0500

    Change 'inc' to 'include'; clean up; add some validation

--- t1apicore/t1service.py
+++ t1apicore/t1service.py
@@ -8,9 +8,11 @@ to parse it. Uses json and cPickle/pickle to serialize cookie objects.
 
 from functools import partial
 from .t1connection import T1Connection
-from .t1atomiccreative import T1AtomicCreative
+from .t1error import T1ClientError
+from .t1adserver import T1AdServer
 from .t1advertiser import T1Advertiser
 from .t1agency import T1Agency
+from .t1atomiccreative import T1AtomicCreative
 from .t1campaign import T1Campaign
 from .t1concept import T1Concept
 from .t1organization import T1Organization
@@ -19,28 +21,30 @@ from .t1strategy import T1Strategy
 from .t1user import T1User
 
 CLASSES = {
-	'atomic_creatives': T1AtomicCreative,
-	'pixel_bundles': T1PixelBundle,
+	'ad_servers': T1AdServer,
 	'advertisers': T1Advertiser,
 	'agencies': T1Agency,
-	'organizations': T1Organization,
+	'atomic_creatives': T1AtomicCreative,
 	'campaigns': T1Campaign,
-	'strategies': T1Strategy,
 	'concepts': T1Concept,
+	'organizations': T1Organization,
 	# 'pixels': T1Pixel,
+	'pixel_bundles': T1PixelBundle,
+	'strategies': T1Strategy,
 	'users': T1User,
 	
 }
 SINGULAR = {
-	'atomic_creative': T1AtomicCreative,
-	'pixel_bundle': T1PixelBundle,
+	'ad_server': T1AdServer,
 	'advertiser': T1Advertiser,
 	'agency': T1Agency,
-	'organization': T1Organization,
+	'atomic_creative': T1AtomicCreative,
 	'campaign': T1Campaign,
-	'strategy': T1Strategy,
 	'concept': T1Concept,
+	'organization': T1Organization,
 	# 'pixel': T1Pixel,
+	'pixel_bundle': T1PixelBundle,
+	'strategy': T1Strategy,
 	'user': T1User,
 }
 
@@ -59,6 +63,7 @@ class T1Service(T1Connection):
 			self.username = username
 		auth = (self.username, self.password)
 		super(T1Service, self).__init__(auth, environment)
+		self._check_session()
 		# self.adama.auth = (self.username, self.password)
 	def __getattr__(self, attr):
 		"""Provides further active-record-like support.
@@ -72,6 +77,9 @@ class T1Service(T1Connection):
 			return partial(self.new, attr)
 		else:
 			raise AttributeError
+
+	def _check_session(self):
+		self._get(self.api_base + '/session')
 
 	# @classmethod # Because we need auth here, can't be class method
 	# def new(cls, collection):
@@ -101,21 +109,25 @@ class T1Service(T1Connection):
 			return CLASSES[ent_type](self.adama.auth, properties=ent_dict)
 
 	def get(self, collection, entity=None, limit=None,
-			inc=None, full=None, sort_by='id', page_offset=0, count=False):
+			include=None, full=None, sort_by='id',
+			page_offset=0, page_limit=100, count=False):
 		url = [self.api_base, collection]
-		clas = CLASSES[collection]
 		if entity is not None:
 			url.append(str(entity)) # str so that we can use join
 			params = {}
 		else:
-			params = {'page_limit': 100, 'page_offset': page_offset,
+			params = {'page_limit': page_limit, 'page_offset': page_offset,
 						'sort_by': sort_by}
 		if isinstance(limit, dict):
-			url.extend(['limit', '%s=%d' % limit.items()[0]])
-		if isinstance(inc, list): # NOT YET IMPLEMENTED
-			params['with'] = ','.join(inc)
-		elif inc is not None:
-			params['with'] = inc
+			if len(limit) != 1:
+				raise T1ClientError('Limit must consist of one parent collection'
+					' (or chained parent collection) and a single value for it'
+					' (e.g. {"advertiser": 1}, or {"advertiser.agency": 2)')
+			url.extend(['limit', '{0!s}={1!d}'.format(*limit.items()[0])])
+		if isinstance(include, list): # Can't use "with" here because keyword
+			params['with'] = ','.join(include)
+		elif include is not None:
+			params['with'] = include
 		if isinstance(full, list):
 			params['full'] = ','.join(full)
 		elif full is not None:
@@ -132,21 +144,24 @@ class T1Service(T1Connection):
 			return entities
 
 	def __get_all(self, collection, limit=None,
-				inc=None, full=None, sort_by='id', count=False):
+				include=None, full=None, sort_by='id', count=False):
 		"""Retrieves all records for a collection or limited collection.
 		
 		ONLY INCLUDED FOR COMPLETENESS. Normally self.get should be used, as
 		T1 will soon reject queries without a page_limit parameter.
 		"""
 		url = [self.api_base, collection]
-		clas = CLASSES[collection]
 		params = {'sort_by': sort_by}
 		if isinstance(limit, dict):
-			url.extend(['limit', '%s=%d' % limit.items()[0]])
-		# if isinstance(inc, list): # NOT YET IMPLEMENTED
-		# 	params['with'] = ','.join(inc)
-		# elif inc is not None:
-		# 	params['with'] = inc
+			if len(limit) != 1:
+				raise T1ClientError('Limit must consist of one parent collection'
+					' (or chained parent collection) and a single value for it'
+					' (e.g. {"advertiser": 1}, or {"advertiser.agency": 2)')
+			url.extend(['limit', '{0!s}={1!d}'.format(*limit.items()[0])])
+		if isinstance(include, list): # Can't use "with" here because keyword
+			params['with'] = ','.join(include)
+		elif include is not None:
+			params['with'] = include
 		if isinstance(full, list):
 			params['full'] = ','.join(full)
 		elif full is not None:
@@ -167,7 +182,6 @@ class T1Service(T1Connection):
 			params['full'] = ','.join(full)
 		elif full is not None:
 			params['full'] = full
-		clas = CLASSES[collection]
 		entities, ent_count = self._get(url, params=params)
 		for index, entity in enumerate(entities):
 			entities[index] = self.return_class(entity)
