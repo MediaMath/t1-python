676704cb32b310f255713dd4ac1b5af97a8d1a49
Nathan Grouse <ngrouse77@gmail.com>
2014-09-10 17:37:45 -0400

    Add docstring for T1TargetDimension save function

--- terminalone/t1targetdimension.py
+++ terminalone/t1targetdimension.py
@@ -34,8 +34,16 @@ class T1TargetDimension(T1SubObject):
 		for index, ent_dict in enumerate(self.include):
 			self.include[index] = T1TargetValue(self.session,
 				properties=ent_dict, environment=self.environment)
-
-	def save(self, data=None, obj_flag=False):
+	
+	def save(self, data=None, obj=False):
+		"""Saves the TargetDimension object.
+		
+		The data keyword expects dictionary of properties to POST to T1.
+		The obj keyword is a flag for passing T1TargetValue objects in the
+		include/exclude field, instead of TargetValue IDs.
+		
+		Arguments are optional.
+		"""
 		if self.properties.get('id'):
 			url = '/'.join([self.api_base, self.parent, str(self.parent_id),
 							self.collection, str(self.id)])
@@ -45,7 +53,7 @@ class T1TargetDimension(T1SubObject):
 		if data is not None:
 			data = self._validate_write(data)
 		else:
-			if obj_flag:
+			if obj:
 				data = {
 					'exclude': [target_value.id for target_value in self.exclude],
 					'include': [target_value.id for target_value in self.include]
