fd1ebd6a9dc367beaa9bab5c8984cbe4a5d415f0
Prasanna Swaminathan <prasanna@mediamath.com>
2014-12-24 13:37:07 -0500

    Increment version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requirements = ['requests>=2.3.0']
 
 setup(
 	name='TerminalOne',
-	version='0.3.13',
+	version='0.3.14',
 	author='Prasanna Swaminathan',
 	author_email='prasanna@mediamath.com',
 	url='http://www.mediamath.com',
--- terminalone/__init__.py
+++ terminalone/__init__.py
@@ -7,7 +7,7 @@ to parse it.
 
 __author__ = 'Prasanna Swaminathan'
 __copyright__ = 'Copyright 2014, Prasanna Swaminathan'
-__version__ = '0.3.13'
+__version__ = '0.3.14'
 __maintainer__ = 'Prasanna Swaminathan'
 __email__ = 'prasanna@mediamath.com'
 __status__ = 'Development'
