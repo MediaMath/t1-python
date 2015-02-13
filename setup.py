dbf4788ccea50b3acafdcc4ce537fcbdd5811543
Prasanna Swaminathan <prasanna@mediamath.com>
2015-02-13 16:14:35 -0500

    Increment version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ requirements = ['requests>=2.3.0']
 
 setup(
 	name='TerminalOne',
-	version='0.4.1',
+	version='0.4.2',
 	author='Prasanna Swaminathan',
 	author_email='prasanna@mediamath.com',
 	url='http://www.mediamath.com',
