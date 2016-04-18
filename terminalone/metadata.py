16ef7c714de9d76dc490025d354399bfa5e8259d
Prasanna Swaminathan <prasanna@pswaminathan.com>
2016-04-18 13:44:30 -0400

    Update version

--- terminalone/metadata.py
+++ terminalone/metadata.py
@@ -3,7 +3,7 @@
 __author__ = 'Prasanna Swaminathan'
 __copyright__ = 'Copyright 2015, MediaMath'
 __license__ = 'BSD'
-__version__ = '1.1.4'
+__version__ = '1.1.6'
 __maintainer__ = 'MediaMath Developer Relations'
 __email__ = 'developers@mediamath.com'
 __status__ = 'Stable'
