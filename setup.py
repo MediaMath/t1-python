15c8201f559f55837ae67cd47415b6f33fab4634
Fod Tzellos <ftzellos@mediamath.com>
2016-12-02 11:00:50 +0000

    Increment version

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ requirements = [
 
 setup(
     name='TerminalOne',
-    version='1.4.6',
+    version='1.5.0',
     author='Prasanna Swaminathan',
     author_email='prasanna@mediamath.com',
     url='http://www.mediamath.com',
