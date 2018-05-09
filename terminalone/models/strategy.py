c2016363d95b86ecffafc55ac06a21ef5f3a336a
aaditih <aaditi.hirave@xoriant.com>
2018-05-09 12:12:31 +0530

    Update strategy.py
    Removing Spaces

--- terminalone/models/strategy.py
+++ terminalone/models/strategy.py
@@ -34,7 +34,7 @@ class Strategy(Entity):
                                  'RTB')
     _type = t1types.enum({'REM', 'GBO', 'AUD'}, 'GBO')
     _alloc_pacing_type = t1types.enum({'monetary', 'impression'}, 'monetary')
-    
+
     _pull = {
         'audience_segment_exclude_op': None,
         'audience_segment_include_op': None,
